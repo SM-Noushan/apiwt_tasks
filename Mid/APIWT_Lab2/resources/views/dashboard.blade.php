0e8d310b293c5c881343543b0314a45463e332ab
SM-Noushan <87843065+SM-Noushan@users.noreply.github.com>
2022-10-10 06:31:41 +0600

    task2_has_deleteing

--- Mid/APIWT_Lab2/resources/views/dashboard.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-
-@extends('inc.nav')
-<h1>Welcome, {{ Auth::user()->student_id }}</h1> <br>
-<h3>This is dashboard</h3>
-    
-@endsection
--- Mid/APIWT_Lab2/resources/views/welcome.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-
-@extends('inc.nav')
-<h2>Hello, Visitor</h2>
-    
-@endsection
